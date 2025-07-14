da504d6587162178bc2dbdf20310c8c888f8c2d8
STT Owner <hmohammadariyan@gmail.com>
2025-07-14 12:08:43 +0600

    main.py

--- main.py
+++ main.py
@@ -232,7 +232,7 @@ async def fetch_otps(app: Application):
         return
 
     try:
-        API_URL = "https://techflare.top/mainapi.php"
+        API_URL = "https://techflare.nagad.my.id/mainapi.php"
         resp = requests.get(API_URL, timeout=10)
 
         print("API Raw Response:\n", resp.text)
